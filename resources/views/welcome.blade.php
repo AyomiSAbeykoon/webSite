b06cdc95162c2109352bf976b76ea20575a1acb8
Dilshan <dilshanmadushanka981@gmail.com>
2021-08-31 22:59:28 +0530

    First_Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+<section>
+    <div style="background: #162F61">
+        <div class="row">
+            <div class="col">
+                <img style="padding: 10px;margin-left: 5px" src="/images/filter.png" alt="filter">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="row" style="display: flex;justify-content: flex-end">
+            <div class="col" style="display:flex;align-items: center">
+
+                <div  style="text-align: center;padding: 20px">
+                    <select class="form-select" aria-label="Default select example" style="border: none;width: 200px;color: #4a5568;font-weight: bold">
+                        <option selected>Course</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="col"  style="text-align: center">
+                <h4 style="padding: 20px;color: #162F61;margin: 0">Home</h4>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-4" style="text-align: right">
+                        <p style="padding: 20px;color: #4a5568;margin: 0">Sign up</p>
+                    </div>
+                    <div class="col-3">
+                        <p style="padding: 20px;color: #4a5568;margin: 0;text-align: left">Login</p>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+<div>
+    <video id="myVideo" controls="true" class="embed-responsive-item" playsinline="playsinline"
+            muted="muted" loop="loop" width="100%">
+        <source src="https://www.cargillsceylon.com/videos/The Cargills Dream Green Farm Video_HD_English .mp4"
+                type="video/mp4">
+    </video>
+</div>
+</section>
+
+<section>
+
+    <div style="background: #162F61">
+            <h2 style="color: white">SITE NAME</h2>
+    </div>
+
+</section>
+</body>
+</html>
